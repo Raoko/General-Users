25ccf6faf01c8dc19a3fecc30bcfc6d1f81101d5
Romeo Patino <Alejandroromeopatino@gmail.com>
2017-09-18 17:29:47 -0500

    Add files via upload

--- Files/login.py
+++ Files/login.py
@@ -17,11 +17,40 @@ class automatelogin():
 
     def __init__(self):
         self.glob = webmate()
-        
+        self.user()
+        self.main()
+
+    def user(self):
+        self.users = [
+                    "general1",
+                    "general2",
+                    "general3"
+                        ]
+        self.usuario = pg.prompt(text="USER:     general1  ,   general2  ,   general3  ", title="Consumer Law Group")
+        if self.usuario in self.users:
+            for i in range(1):
+                contrasena = pg.password("Password")
+                if contrasena == "consumer123":
+                    break
+                else:
+                    x = pg.confirm(text="Wrong Username/Password", title="Consumer Law Group", buttons=["Try again", "Quit"])
+                    if x == "Try again":
+                        self.user()
+                    else:
+                        quit()
+                    
+        else:
+            y = pg.confirm(text="Wrong Username/Password", title="Consumer Law Group", buttons=["Try again", "Quit"])
+            if y == "Try again":
+                self.user()
+            else:
+                quit()
+                
+              
     def main(self):
         while True:
             sleep = 0.1 #seconds of sleep
-            self.startButton = pg.confirm(text='\n\n\nCONSUMER   LAW   GROUP\n\n', title='CLG USER: GENERAL 1',buttons=['\n    Office    \n', '\nPhone System\n', '\n     Logic     \n', '\nEmail(Correo)\n'])
+            self.startButton = pg.confirm(text='\n\n\nCLG USER:     ' + self.usuario + '\n\n', title='CONSUMER   LAW   GROUP',buttons=['\n    Office    \n', '\nPhone System\n', '\n     Logic     \n', '\nEmail(Correo)\n'])
             if self.startButton == '\n    Office    \n':
                 self.office()
                 time.sleep(sleep)
@@ -45,7 +74,7 @@ class automatelogin():
         self.glob.kill('chrome.exe')
         self.officelogin = webmate("http://office.yourclg.com/")
         self.officelogin.loadDriver(PHANTOM=False)
-        self.officelogin.formInput(XPATH='//*[@id="edit-name"]', KEY='general1')
+        self.officelogin.formInput(XPATH='//*[@id="edit-name"]', KEY=self.usuario)
         self.officelogin.formInput(XPATH='//*[@id="edit-pass"]', KEY='clg1234!')
         self.officelogin.buttonClick(XPATH='//*[@id="edit-submit"]')
         self.glob.kill('chromedriver.exe')
@@ -54,7 +83,7 @@ class automatelogin():
         self.glob.kill('chrome.exe')
         self.phonesystemlogin = webmate('http://admin.phonesystem.yourclg.com/cti/')
         self.phonesystemlogin.loadDriver(PHANTOM=False)
-        self.phonesystemlogin.formInput(XPATH='//*[@id="username"]', KEY='general1')
+        self.phonesystemlogin.formInput(XPATH='//*[@id="username"]', KEY=self.usuario)
         self.phonesystemlogin.formInput(XPATH='//*[@id="password"]', KEY='clg1234!')
         self.phonesystemlogin.buttonClick(XPATH='//*[@id="button"]')
         self.glob.kill('chromedriver.exe')
@@ -63,7 +92,7 @@ class automatelogin():
         self.glob.kill('chrome.exe')
         self.logiclogin = webmate('https://clg.irslogics.com/')
         self.logiclogin.loadDriver(PHANTOM=False)
-        self.logiclogin.formInput(XPATH='//*[@id="txtUsername2"]', KEY='general1@consumerlaw.com')
+        self.logiclogin.formInput(XPATH='//*[@id="txtUsername2"]', KEY=self.usuario + "@consumerlaw.com")
         self.logiclogin.formInput(XPATH='//*[@id="txtPassword2"]', KEY='Santana007!')
         self.logiclogin.buttonClick(XPATH='//*[@id="btnLogin2"]')
         self.glob.kill('chromedriver.exe')
@@ -72,10 +101,10 @@ class automatelogin():
         self.glob.kill('chrome.exe')
         self.emaillogin = webmate('https://goo.gl/qixfyG')
         self.emaillogin.loadDriver(PHANTOM='False')
-        self.emaillogin.formInput(XPATH='//*[@id="cred_userid_inputtext"]', KEY='general1@consumerlaw.com')
+        self.emaillogin.formInput(XPATH='//*[@id="cred_userid_inputtext"]', KEY=self.usuario + "@consumerlaw.com")
         self.emaillogin.formInput(XPATH='//*[@id="cred_password_inputtext"]', KEY='Santana007!', pressEnter=True)
         self.glob.kill('chromedriver.exe')
 
 if __name__ == '__main__':
     log = automatelogin()
-    log.main()
+
