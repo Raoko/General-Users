69e9ed87bfb933c4d7bd19159e43eb87ce66b4dc
Romeo Patino <Alejandroromeopatino@gmail.com>
2017-09-18 12:24:02 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 # setup.py
 from distutils.core import setup
 import py2exe
+import Files
 
-setup(windows=["login.py", "install_driver.py"],
+wd_path = 'C:\\Python27\\Lib\\site-packages\\selenium\\webdriver'
+required_data_files = [('selenium/webdriver/remote',['{}\\remote\\getAttribute.js'.format(wd_path),'{}\\remote\\isDisplayed.js'.format(wd_path)])]
+
+setup(
+    windows=["Files\login.py", "Files\install_driver.py"],
+     data_files = required_data_files,
       options={
           'py2exe':{
               'packages': ['selenium', 'Webmate', 'pyautogui'],
-              #"skip_archive":True,
-              #"unbuffered": True,
-              #"optimize":2
+              "skip_archive":True,
+              "unbuffered": True,
+              "optimize":2
               }
           }
       )
