bac943b954987eb36c6e941767b5bbe2b5b2b453
Romeo Patino <Alejandroromeopatino@gmail.com>
2017-09-20 12:25:49 -0500

    setup for others

--- setup.py
+++ setup.py
@@ -12,8 +12,10 @@ import Files
 import subprocess
 import time
 #Start
-print "STARTING DOWNLOAD\n\n"
-print "Please dont close console\n\n"
+print "\n\n     1.) COMPILING FILES...\n\n"
+time.sleep(2)
+print "     Please dont close console\n\n"
+time.sleep(3)
 wd_path = 'C:\\Python27\\Lib\\site-packages\\selenium\\webdriver'
 required_data_files = [('selenium/webdriver/remote',['{}\\remote\\getAttribute.js'.format(wd_path),'{}\\remote\\isDisplayed.js'.format(wd_path)])]
 
@@ -34,14 +36,19 @@ setup(
           }
       )
 
-print "\nINSTALLING DRIVER...\n\n"
+time.sleep(1)
+print "\n       2.) INSTALLING DRIVER...\n\n"
+time.sleep(2)
 setup(windows=["Files\install_driver.py"])
 #opens driver
 #WINDOWS ONLY
 sleep = 3
+print "\n       3.) UNZIPPING FILES...\n"
 time.sleep(sleep)
 subprocess.call(["dist\install_driver.exe"])
-print "\nDOWNLOAD COMPLETE!!\n\n\n"
+time.sleep(1)
+print "\n       DOWNLOAD COMPLETE!!!\n\n"
+time.sleep(1)
 #Done
                  
 
