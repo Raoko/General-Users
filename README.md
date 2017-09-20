980761d4783837636226fe0ffc76596435ff6935
Romeo Patino <Alejandroromeopatino@gmail.com>
2017-09-20 11:18:10 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Automatic Login
-2017
-Created by Romeo Patino
-rptino@consumerlaw.com
-# Download Setup : https://www.dropbox.com/s/vdg5pn6is8rfllt/CLG_Setup.exe?dl=0
+# ````````````````
+# 2017
+# Created by Romeo Patino
+# rptino@consumerlaw.com
+#
+# Download File : https://www.dropbox.com/s/4ltc5q6rg5zuxmo/CLGGeneral.zip?dl=0 
--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
 # setup.py
+#DEVELOPER ONLY
 from distutils.core import setup
 import py2exe
 import Files
-import os
+import subprocess
+import time
 
 wd_path = 'C:\\Python27\\Lib\\site-packages\\selenium\\webdriver'
 required_data_files = [('selenium/webdriver/remote',['{}\\remote\\getAttribute.js'.format(wd_path),'{}\\remote\\isDisplayed.js'.format(wd_path)])]
 
 setup(
     windows=["Files\login.py", "Files\install_driver.py"],
-     data_files = required_data_files,
-      options={
-          'py2exe':{
-              'packages': ['selenium', 'Webmate', 'pyautogui'],
-              "skip_archive":True,
-              "unbuffered": True,
-              "optimize":2
+             data_files = required_data_files,
+              options={
+                  'py2exe':{
+                      'packages': ['selenium', 'Webmate', 'pyautogui'],
+                      "skip_archive":True,
+                      "unbuffered": True,
+                      "optimize":2
               }
           }
       )
 
+#opens driver
+#WINDOWS ONLY
+sleep = 3
+time.sleep(sleep)
+subprocess.call(["dist\install_driver.exe"])
+                 
 
 
